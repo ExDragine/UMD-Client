9a8782c7968b9a863166818ef4565391c99febdc
ExDragine <surfaceluck@outlook.com>
2024-06-17 18:53:23 +0800

    format other file

--- interface/SGP40.py
+++ interface/SGP40.py
@@ -6,17 +6,17 @@ import time
 import math
 import struct
 import smbus
+
 # import ctypes
 
 # voc = ctypes.cdll.LoadLibrary('./voclib.so')
 
 SGP40_CMD_FEATURE_SET = [0x20, 0x2F]
-SGP40_CMD_MEASURE_TEST = [0X28,0X0E]
-SGP40_CMD_SOFT_RESET = [0X00,0X06]
-SGP40_CMD_HEATER_OFF = [0X36,0X15]
-SGP40_CMD_MEASURE_RAW = [0X26,0X0F]
+SGP40_CMD_MEASURE_TEST = [0x28, 0x0E]
+SGP40_CMD_SOFT_RESET = [0x00, 0x06]
+SGP40_CMD_HEATER_OFF = [0x36, 0x15]
+SGP40_CMD_MEASURE_RAW = [0x26, 0x0F]
 # SGP40_CMD_GET_SERIAL_ID = [0X36,0X82] #DATASHEET is not written ,but Sensirion have
-
 
 CRC_TABLE = [
         0, 49, 98, 83, 196, 245, 166, 151, 185, 136, 219, 234, 125, 76, 31, 46,
@@ -37,81 +37,82 @@ CRC_TABLE = [
         130, 179, 224, 209, 70, 119, 36, 21, 59, 10, 89, 104, 255, 206, 157, 172
         ]
 
-#Without_humidity_compensation
-#sgp40_measure_raw + 2*humi + CRC + 2*temp + CRC
-WITHOUT_HUM_COMP = [0X26, 0X0F, 0X80, 0X00, 0XA2, 0X66, 0X66, 0X93] # default Temperature=25 Humidity=50
-WITH_HUM_COMP = [0x26, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00] #Manual input
+# Without_humidity_compensation
+# sgp40_measure_raw + 2*humi + CRC + 2*temp + CRC
+WITHOUT_HUM_COMP = [0x26, 0x0F, 0x80, 0x00, 0xA2, 0x66, 0x66, 0x93]  # default Temperature=25 Humidity=50
+WITH_HUM_COMP = [0x26, 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  # Manual input
 
 ADDR = 0x59
+
 
 class SGP40:
     def __init__(self, address=ADDR):
         self.i2c = smbus.SMBus(1)
         self.address = address
-        
+
         # feature set 0x3240/0x3220
-        self.write(SGP40_CMD_FEATURE_SET)    
+        self.write(SGP40_CMD_FEATURE_SET)
         time.sleep(0.25)
-        Rbuf = self.Read() 
+        Rbuf = self.Read()
         # print('feature set:%#x'% ((int(Rbuf[0]) << 8) | Rbuf[1]))
         if ((int(Rbuf[0]) << 8) | Rbuf[1]) != 0x3240 and ((int(Rbuf[0]) << 8) | Rbuf[1]) != 0x3220:
             raise RuntimeError("Self test failed")
-        
-        # Self Test 0xD400      
-        self.write(SGP40_CMD_MEASURE_TEST)    
+
+        # Self Test 0xD400
+        self.write(SGP40_CMD_MEASURE_TEST)
         time.sleep(0.32)
         Rbuf = self.Read()
         # print('Self Test  :%#x'% ((int(Rbuf[0]) << 8) | Rbuf[1]))
-        if ((int(Rbuf[0]) << 8) | Rbuf[1]) != 0xD400: #0x4B00 is failed,0xD400 pass
+        if ((int(Rbuf[0]) << 8) | Rbuf[1]) != 0xD400:  # 0x4B00 is failed,0xD400 pass
             raise RuntimeError("Self test failed")
-            
+
         # reset
         # self.write(SGP40_CMD_SOFT_RESET)
 
     def Read(self):
-        return self.i2c.read_i2c_block_data(self.address, 0, 3)#last byte is CRC8
-    
+        return self.i2c.read_i2c_block_data(self.address, 0, 3)  # last byte is CRC8
+
     def write(self, cmd):
         self.i2c.write_byte_data(self.address, cmd[0], cmd[1])
-        
+
     def write_block(self, cmd):
         self.i2c.write_i2c_block_data(self.address, cmd[0], cmd[1:8])
-        
+
     def raw(self):
         """The raw gas value"""
         # recycle a single buffer
         self.write_block(WITHOUT_HUM_COMP)
         time.sleep(0.25)
         Rbuf = self.Read()
-        return ((int(Rbuf[0]) << 8) | Rbuf[1])
-        
+        return (int(Rbuf[0]) << 8) | Rbuf[1]
+
     def measureRaw(self, temperature, humidity):
         # 2*humi + CRC
-        #paramh = struct.pack(">H", math.ceil(humidity * 0xffff / 100))
-        h = humidity * 0xffff / 100
-        paramh = (h >> 8, h & 0xff)
+        # paramh = struct.pack(">H", math.ceil(humidity * 0xffff / 100))
+        h = humidity * 0xFFFF / 100
+        paramh = (h >> 8, h & 0xFF)
         crch = self.__crc(paramh[0], paramh[1])
-        
+
         # 2*temp + CRC
-        #paramt = struct.pack(">H", math.ceil((temperature + 45) * 0xffff / 175))
-        t = (temperature + 45) * 0xffff / 175
-        paramt = (t >> 8, t & 0xff)
+        # paramt = struct.pack(">H", math.ceil((temperature + 45) * 0xffff / 175))
+        t = (temperature + 45) * 0xFFFF / 175
+        paramt = (t >> 8, t & 0xFF)
         crct = self.__crc(paramt[0], paramt[1])
-        
+
         WITH_HUM_COMP[2:3] = paramh
         WITH_HUM_COMP[4] = int(crch)
         WITH_HUM_COMP[5:6] = paramt
         WITH_HUM_COMP[7] = int(crct)
-        #print(WITH_HUM_COMP)
+        # print(WITH_HUM_COMP)
         self.write_block(WITH_HUM_COMP)
-        
+
         time.sleep(0.5)
         Rbuf = self.Read()
         # print(Rbuf)
-        return ((int(Rbuf[0]) << 8) | Rbuf[1])
-        
+        return (int(Rbuf[0]) << 8) | Rbuf[1]
+
     def __crc(self, msb, lsb):
-        crc = 0xff
+        crc = 0xFF
         crc ^= msb
         crc = CRC_TABLE[crc]
         if lsb is not None:
@@ -119,14 +120,15 @@ class SGP40:
             crc = CRC_TABLE[crc]
         return crc
 
+
 if __name__ == '__main__':
     sgp = SGP40()
     time.sleep(1)
     try:
         while True:
             # print("Raw Gas: ", sgp.raw())
-            print("measureRaw Gas: %d" %sgp.measureRaw(25, 50))
-            
+            print("measureRaw Gas: %d" % sgp.measureRaw(25, 50))
+
             time.sleep(1)
 
     except KeyboardInterrupt:
